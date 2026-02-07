ad5be0b911a7640ffcecb5f3db972b47cbb22617
Prachi Sharma <147993724+prachiswrn15@users.noreply.github.com>
2026-02-07 18:43:40 +0530

    Refactor ViewMessages component for cleaner code

--- src/components/ViewMessage.js
+++ src/components/ViewMessage.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../App.css";
-
 const ViewMessages = () => {
   const [messages, setMessages] = useState([]);
-
   useEffect(() => {
     axios.get("http://localhost:8080/api/contact")
       .then((res) => setMessages(res.data))
       .catch((err) => console.error(err));
   }, []);
-
   return (
     <div className="messages-wrapper">
       <h2 style={{ color: "crimson" }}>All Contact Messages</h2>
@@ -37,5 +34,4 @@ const ViewMessages = () => {
     </div>
   );
 };
-
 export default ViewMessages;
